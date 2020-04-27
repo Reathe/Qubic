9a5e6a9cf290e5ab152a15a18bd3332cea13489d
Bachourian Rafael <bachourian@gmail.com>
2020-04-27 14:48:33 +0200

    Mouse controls pas deg
    ammend: fix mouse controls 
     toggle

--- src/UI/Qubic/vue_qubic.py
+++ src/UI/Qubic/vue_qubic.py
@@ -4,38 +4,61 @@ from ursina import *
 
 from controls import Controls
 from qubic_observer import QubicObserver
+from qubic_subject import QubicSubject
 from ui.qamera.qamera_locked import QameraLocked
 from ui.qubic.vue_pion import VuePion, VuePionFactory
 from composite import Composite
 
 
-class _Sol(Button):
-	def __init__(self, qubic, **kwargs):
-		self.qubic = qubic
-		super().__init__(
-			model='cube',
-			texture='white_cube',
-			origin=(0, 0.5),
-			color=color.white,
-			**kwargs
-		)
-		self.scale_y = 0.5
-		self.y = self.y * self.scale[1]
-		self.__next = self.__controle_on_click, self.color.tint(-0.2), self.color.tint(-0.4), 1
-		self.on_click, self.highlight_color, self.pressed_color, self.pressed_scale = None, self.color, self.color, 1
-		self.toggle_on_click()
+class _Floor(Composite):
+	class __Floor1(QubicSubject, Button):
+		def __init__(self, qubic, **kwargs):
+			self.qubic = qubic
+			super().__init__(
+				model='cube',
+				texture='white_cube',
+				origin=(0, 0.5),
+				color=color.white,
+				**kwargs
+			)
+			self.scale_y = 0.5
+			self.y = self.y * self.scale[1]
+			self.__next = self.__controle_on_click, self.color.tint(-0.2), self.color.tint(-0.4), 1
+			self.on_click, self.highlight_color, self.pressed_color, self.pressed_scale = None, self.color, self.color, 1
 
-	def __controle_on_click(self):
-		pos = tuple(self.position + Vec3(0, len(self.qubic), 0))
-		pos = int(pos[0]), int(pos[1]), int(pos[2])
-		pos = self.qubic.get_pos_with_gravity(pos)
-		self.qubic.poser(pos)
+		def __controle_on_click(self):
+			pos = tuple(self.position + Vec3(0, len(self.qubic), 0))
+			pos = int(pos[0]), int(pos[1]), int(pos[2])
+			pos = self.qubic.get_pos_with_gravity(pos)
+			self.qubic.poser(pos)
+			self.notify_observers()
+
+		def toggle_on_click(self):
+			temp = self.__next
+			on_click = None if temp[0] else self.__controle_on_click
+			self.__next = on_click, self.highlight_color, self.pressed_color, self.pressed_scale
+			self.on_click, self.highlight_color, self.pressed_color, self.pressed_scale = temp
+
+		def on_mouse_enter(self):
+			super().on_mouse_enter()
+			self.notify_observers()
+
+		def notify_observers(self):
+			self.observers[0].notify(self.position)
+
+	def __init__(self, qubic, *args, **kwargs):
+		super().__init__(*args, **kwargs)
+		for z in range(len(qubic)):
+			for x in range(len(qubic)):
+				self.components.append(self.__Floor1(qubic, position=(x, 0, z), parent=self))
 
 	def toggle_on_click(self):
-		temp = self.__next
-		on_click = None if temp[0] else self.__controle_on_click
-		self.__next = on_click, self.highlight_color, self.pressed_color, self.pressed_scale
-		self.on_click, self.highlight_color, self.pressed_color, self.pressed_scale = temp
+		for sol in self.components:
+			sol.toggle_on_click()
+
+	def add_observers(self, *observers):
+		for sol in self.components:
+			sol.add_observers(*observers)
 
 
 class _VueQubicSettings:
@@ -43,7 +66,7 @@ class _VueQubicSettings:
 		super().__init__()
 		self.center = None
 		self.vue_pion = 'Classic'
-		self.control_method = 'Keyboard'
+		self.control_method = 'Mouse'
 		self.qamera_type = QameraLocked
 
 
@@ -53,21 +76,19 @@ class VueQubic(Composite, QubicObserver):
 	def __init__(self, qubic, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 		taille = len(qubic)
-
 		self.qubic = qubic
 		qubic.add_observers(self)
 		# TODO: controls depending on camera's position/angle
 		self.settings = _VueQubicSettings()
 		target = (taille / 2 - .5, 0, taille / 2 - .5)
 		self.qamera = self.settings.qamera_type(target)
+		sol = _Floor(qubic)
+		self.components.append(sol)
 		controls_type = Controls.get_controls(self.settings.control_method)
-		self.controls = controls_type(qubic, self.settings.vue_pion)
+		self.controls = controls_type(qubic, self)
 		self.components.append(self.controls)
-
+		sol.add_observers(self.controls)
 		self.pions = [[[None for _ in range(taille)] for _ in range(taille)] for _ in range(taille)]
-		for z in range(taille):
-			for x in range(taille):
-				self.components.append(_Sol(qubic, position=(x, 0, z), parent=scene))
 
 	# print("rota: {}".format(self.plateau))
 
--- src/controls.py
+++ src/controls.py
@@ -1,18 +1,17 @@
-from abc import ABC
-
 from composite import Composite
 from model.curseur import Curseur
 from model.direction_tools import DERRIERE, DEVANT, GAUCHE, DROITE
+from qubic_observer import QubicObserver
 from ui.qubic.vue_pion import VuePionFactory
 
 
-class Controls(Composite, ABC):
+class Controls(QubicObserver, Composite):
 	def __init__(self, qubic, vue, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 		self.qubic = qubic
 		taille = self.qubic.taille
 		self.curseur = Curseur((taille,) * 3)
-		self.vue_curseur = VuePionFactory(qubic, vue).create_pion((0, 0, 0))
+		self.vue_curseur = VuePionFactory(qubic, vue.settings.vue_pion).create_pion((0, 0, 0))
 		self.components.append(self.vue_curseur)
 
 	@staticmethod
@@ -21,6 +20,13 @@ class Controls(Composite, ABC):
 		        'Mouse': ControlsMouse
 		        }
 		return link.get(controls_type)
+
+	def maj_vue(self):
+		self.vue_curseur.position = self.curseur.pos
+		self.vue_curseur.y *= 0.5
+
+	def notify(self, truc):
+		pass
 
 
 class ControlsKeyboard(Controls):
@@ -46,11 +52,18 @@ class ControlsKeyboard(Controls):
 			self.curseur.pos = self.qubic.get_pos_with_gravity(self.curseur)
 			self.maj_vue()
 
-	def maj_vue(self):
-		self.vue_curseur.position = self.curseur.pos
-		self.vue_curseur.y *= 0.5
-
 
 class ControlsMouse(Controls):
-	def __init__(self, qubic, *args, **kwargs):
-		super().__init__(qubic, *args, **kwargs)
+	def __init__(self, qubic, vue, *args, **kwargs):
+		super().__init__(qubic, vue, *args, **kwargs)
+		for c in vue.components:
+			if hasattr(c, 'toggle_on_click'):
+				try:
+					c.toggle_on_click()
+				except Exception as ex:
+					print(f"error on toggling buttons: {ex}")
+
+	def notify(self, position):
+		self.curseur.pos = int(position[0]), self.qubic.taille - 1, int(position[2])
+		self.curseur.pos = self.qubic.get_pos_with_gravity(self.curseur)
+		self.maj_vue()
